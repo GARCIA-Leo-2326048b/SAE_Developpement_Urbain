422777435e49f9daeec40773e6a39bd6de22f020
t22018451 <antoine.thevenet@etu.univ-amu.fr>
2024-10-11 16:17:12 +0200

    Comparaison.php fonctionel: pas encore d'entrée

--- modules/blog/controllers/Comparaison.php
+++ modules/blog/controllers/Comparaison.php
@@ -1,9 +1,18 @@
 <?php
 
 namespace blog\controllers;
+use GeoPHP;
 
 class Comparaison{
-    class function CompareVec(){
+    public function CompareVec(){
+
+        // Charger le GeoJSON
+        $polygone = '{"type":"Polygon","coordinates":[[[30,10],[40,40],[20,40],[10,20],[30,10]]]}';
+        $geometrie = GeoPHP::load($polygone, 'json');
+
+        // Calculer l'aire
+        $area = $geometrie->area();
+        echo "Aire du polygone: " . $area;
 
     }
 
