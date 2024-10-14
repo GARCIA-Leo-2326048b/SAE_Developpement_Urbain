ad2d2ee48864863ee182850b4d99c6560408b06c
Garcia-Leo09 <leo.garcia.5@etu.univ-amu.fr>
2024-10-14 18:40:10 +0200

    Ajustement de l'autoloader

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit0c6f8a8f1b163c98dc2243c4ea3f9525
 {
+    public static $prefixLengthsPsr4 = array (
+        'b' => 
+        array (
+            'blog\\' => 5,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'blog\\' => 
+        array (
+            0 => __DIR__ . '/../..' . '/modules/blog',
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'geoPHP' => __DIR__ . '/..' . '/phayes/geophp/geoPHP.inc',
@@ -14,6 +28,8 @@ class ComposerStaticInit0c6f8a8f1b163c98dc2243c4ea3f9525
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit0c6f8a8f1b163c98dc2243c4ea3f9525::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit0c6f8a8f1b163c98dc2243c4ea3f9525::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInit0c6f8a8f1b163c98dc2243c4ea3f9525::$classMap;
 
         }, null, ClassLoader::class);
