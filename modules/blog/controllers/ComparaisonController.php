237a05b46f198381ddd8a5f7f9a812615a4a84ad
Antoine <antoine.thevenet@etu.univ-amu.fr>
2024-10-16 15:21:49 +0200

    refonte des comparaisons

--- modules/blog/controllers/ComparaisonController.php
+++ modules/blog/controllers/ComparaisonController.php
@@ -10,21 +10,53 @@ class ComparaisonController{
         $geometrieSim = GeoPHP::load($polygoneSim, 'json');
         $geometrieVer = GeoPHP::load($polygoneVer, 'json');
 
-        // Calculer l'aire de la simulation
-        $aireSim = $geometrieSim->area();
-        echo "Aire de la simulation: " . $aireSim;
-        // Calculer l'aire de la vérité terrain
-        $aireVer = $geometrieVer->area();
-        echo "Aire de la vérité terrain: " . $aireVer;
 
-        // Calculer le périmètre de la simulation
-        $perimetreSim = $geometrieSim->length();
-        echo "Périmètre de la simulation: " . $perimetreSim;
-        // Calculer le périmètre de la vérité terrain
-        $perimetreVer = $geometrieVer->length();
-        echo "Périmètre de la vérité terrain: " . $perimetreVer;
+        // PARTIE SIMULATION
 
 
+        // initialisser l'aire totale et le nombre de maisons de la simulation
+        $airettlSim = 0;
+        $nbmaisonSim = 0;
+
+        // calculer l'aire totale et le nombre de maisons de la simulation
+        foreach ($geometrieSim->getComponents() as $maisonSim) {
+            if ($maisonSim->geometryType() === 'Polygon') {
+                $airettlSim += $maisonSim->area();
+                $nbmaisonSim++;
+            }
+        }
+
+        // Calculer l'aire moyenne des maisons de la simulation
+        if ($nbmaisonSim > 0) {
+            $airemoySim = $airettlSim / $nbmaisonSim;
+            echo "Aire moyenne des maisons dans la simulation: " . $airemoySim;
+        } else {
+            echo "Le fichier GeoJSON n'a pas été détecté ou ne contient pas de polygones";
+        }
+
+
+        // PARTIE VERITE TERRAIN
+
+
+        // initialisser l'aire totale et le nombre de maisons de la vérité terrain
+        $airettlVer = 0;
+        $nbmaisonVer = 0;
+
+        // calculer l'aire totale et le nombre de maisons de la vérité terrain
+        foreach ($geometrieVer->getComponents() as $maisonVer) {
+            if ($maisonVer->geometryType() === 'Polygon') {
+                $airettlVer += $maisonVer->area();
+                $nbmaisonVer++;
+            }
+        }
+
+        // Calculer l'aire moyenne des maisons de la vérité terrain
+        if ($nbmaisonVer > 0) {
+            $airemoyVer = $airettlVer / $nbmaisonVer;
+            echo "Aire moyenne des maisons dans la vérité terrain: " . $airemoyVer;
+        } else {
+            echo "Le fichier GeoJSON n'a pas été détecté ou ne contient pas de polygones";
+        }
 
     }
 
