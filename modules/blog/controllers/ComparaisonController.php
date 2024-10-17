ee6e2320b87c97d82313babc54ffe48d8a38ff1c
GARY-Felicite-2326050aa <felicite.gary@etu.univ-amu.fr>
2024-10-17 15:51:20 +0200

    debut des comparaisons sur le shapeIndex

--- modules/blog/controllers/ComparaisonController.php
+++ modules/blog/controllers/ComparaisonController.php
@@ -23,16 +23,31 @@ class ComparaisonController{
         $geometrySim = $this->loadGeoJson($polygonSim);
         $geometryVer = $this->loadGeoJson($polygonVer);
 
-        $areaStatsSim = $this->getAreaStat($geometrySim);
-        $areaStatsVer = $this->getAreaStat($geometryVer);
+        // Récupérer les aires et périmètres pour simulation et vérité terrain
+        $valuesSim = $this->getAreasAndPerimeters($geometrySim);
+        $valuesVer = $this->getAreasAndPerimeters($geometryVer);
 
-        $this->view->showComparison([
-            'sim' => $areaStatsSim,
-            'ver' => $areaStatsVer
-        ]);
+        // Calculer les statistiques pour les aires
+         $areaStatsSim = $this->getStat($valuesSim['areas']);
+        $areaStatsVer = $this->getStat($valuesVer['areas']);
+
+        // Calculer les Shape Index pour simulation et vérité terrain
+        $shapeIndexesSim = $this->getShapeIndexStats(['areas' => $valuesSim['areas'], 'perimeters' => $valuesSim['perimeters']]);
+        $shapeIndexesVer = $this->getShapeIndexStats(['areas' => $valuesVer['areas'], 'perimeters' => $valuesVer['perimeters']]);
+
+        // Calculer les statistiques pour les Shape Index
+        $shapeIndexStatsSim = $this->getStat($shapeIndexesSim);
+        $shapeIndexStatsVer = $this->getStat($shapeIndexesVer);
+
+        $results = [
+            'areaStatsSim' => $areaStatsSim,
+            'areaStatsVer' => $areaStatsVer,
+            'shapeIndexStatsSim' => $shapeIndexStatsSim,
+            'shapeIndexStatsVer' => $shapeIndexStatsVer
+        ];
+        $this->view->showComparison($results);
     }
 
-//a
     private function loadGeoJson($geoJsonData) {
         $geometry = GeoPHP::load($geoJsonData, 'json');
         if (!$geometry) {
@@ -41,23 +56,46 @@ class ComparaisonController{
         return $geometry;
     }
 
-
-    private function getAreaStat($geometry) {
+    private function getAreasAndPerimeters($geometry){
         $areas = [];
+        $perimeters = [];
         //on rentre les aires de tous les batiments dans un tableau
         foreach ($geometry->getComponents() as $component) {
             if ($component->geometryType() === 'Polygon') {
-                $areas[]=$component->area();
+                $areas[] = $component->area();
+                $perimeters[] = $component->length();
             }
         }
-        if (count($areas) > 0) {
-            $mean = array_sum($areas)/count($areas);//moyenne des aires
-            $min = min($areas);//aire minimum
-            $max = max($areas);//aire maximum
-            $std = $this->calculateStandardDeviation($areas,$mean);//ecart-type
+        return [
+            'areas'=>$areas,
+            'perimeters'=>$perimeters];
+        }
+
+
+    private function getShapeIndexStats($polygon)
+    {
+        $shapeIndexes = [];
+        foreach ($polygon['areas'] as $i => $area) {
+            if ($area > 0) {
+                $shapeIndex = $polygon['perimeters'][$i] / (2 * sqrt(pi() * $area));
+                $shapeIndexes[] = $shapeIndex;
+            }
+
+        }
+        return $shapeIndexes;
+    }
+    private function getStat($values) {
+
+        if (count($values) > 0) {
+            $mean = array_sum($values)/count($values);//moyenne des aires
+            $min = min($values);//aire minimum
+            $max = max($values);//aire maximum
+            $std = $this->calculateStandardDeviation($values,$mean);//ecart-type
         } else {
             $mean =$max=$min=$std= 0;
         }
+
+
         return [
             'mean' => $mean,
             'min' => $min,
